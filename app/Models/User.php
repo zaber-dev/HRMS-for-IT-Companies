8707c1698b33deca3c056068f88257198aa6e725
Mahedi Zaman Zaber <88625958+zaber-dev@users.noreply.github.com>
2026-05-05 00:58:46 +0600

    feat: add HasRoles trait and RBAC fields to User model
    - Add Spatie HasRoles trait to User model
    - Add is_active and must_change_password to #[Fillable] attribute
    - Add boolean casts for is_active and must_change_password
    - Preserve existing PHP 8 attribute-based style
    
    Requirements: 1.1, 3.5

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,13 +10,14 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Fortify\TwoFactorAuthenticatable;
+use Spatie\Permission\Traits\HasRoles;
 
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'is_active', 'must_change_password'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable
 {
     /** @use HasFactory<UserFactory> */
-    use HasFactory, Notifiable, TwoFactorAuthenticatable;
+    use HasFactory, HasRoles, Notifiable, TwoFactorAuthenticatable;
 
     /**
      * Get the attributes that should be cast.
@@ -29,6 +30,8 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
             'two_factor_confirmed_at' => 'datetime',
+            'is_active' => 'boolean',
+            'must_change_password' => 'boolean',
         ];
     }
 }
